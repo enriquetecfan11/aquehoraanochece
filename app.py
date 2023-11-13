34c259f748ccbf5d1109af22d88073088cef20f2
Kike Rodriguez Vela <enriquetecfan@Kikes-MacBook-Pro.local>
2023-11-13 21:56:02 +0100

    fixed time

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
+from datetime import datetime, timedelta
 import requests
-from datetime import datetime
 
 # Página principal
 st.title("¿A qué hora anochece?")
@@ -8,11 +8,21 @@ st.title("¿A qué hora anochece?")
 # Agregar un campo de búsqueda de ubicación
 ubicacion = st.text_input("Ingrese su ubicación:")
 
-# Obtener la ubicación del usuario
-if st.button("Detectar ubicación"):
-    # Aquí puedes usar algún servicio de geolocalización o simplemente pedir al usuario que ingrese manualmente
-    # En este ejemplo, asumimos que el usuario ingresa la ubicación manualmente
-    st.warning("¡La detección automática de ubicación no está implementada en este ejemplo!")
+# Obtener latitud y longitud de la ubicación ingresada
+if ubicacion:
+    try:
+        response = requests.get(f"https://nominatim.openstreetmap.org/search?format=json&q={ubicacion}")
+        data = response.json()
+        lat = data[0]["lat"]
+        lon = data[0]["lon"]
+        # Only get the first result
+        # Only get 2 decimal places
+        latnew = round(float(lat), 2)
+        lonnew = round(float(lon), 4)
+        st.success(f"Las coordenadas de {ubicacion} son ({latnew}, {lonnew})")
+
+    except Exception as e:  
+        st.error(f"Error al obtener las coordenadas: {e}")
 
 # Agregar un campo de fecha
 fecha = st.date_input("Seleccione la fecha:", datetime.today())
@@ -20,11 +30,16 @@ fecha = st.date_input("Seleccione la fecha:", datetime.today())
 # Botón para obtener la hora de la puesta de sol
 if st.button("Obtener hora de la puesta de sol"):
     # Construir la URL para la API de sunrise-sunset.org
-    api_url = f"https://api.sunrise-sunset.org/json?lat=0&lng=0&date={fecha.strftime('%Y-%m-%d')}&formatted=0"
+    api_url = f"https://api.sunrise-sunset.org/json?lat={lat}&lng={lon}&date={fecha.strftime('%Y-%m-%d')}&formatted=0"
+
+    # Verificar si la ubicación no está vacía
     if ubicacion:
-        # Si se proporciona la ubicación, intentar obtener las coordenadas usando algún servicio de geocodificación
-        # En este ejemplo, simplemente usamos 0,0 como coordenadas si no hay detección de ubicación.
-        api_url = f"https://api.sunrise-sunset.org/json?lat=0&lng=0&date={fecha.strftime('%Y-%m-%d')}&formatted=0"
+        # Intentar obtener las coordenadas usando algún servicio de geocodificación
+        try:
+            response = requests.get(f"https://nominatim.openstreetmap.org/search?format=json&q={ubicacion}")
+            data = response.json()
+        except Exception as e:
+            st.error(f"Error al obtener las coordenadas: {e}")
 
     # Hacer la solicitud a la API
     response = requests.get(api_url)
@@ -32,7 +47,22 @@ if st.button("Obtener hora de la puesta de sol"):
 
     # Verificar si la solicitud fue exitosa
     if data["status"] == "OK":
-        sunset_time = data["results"]["sunset"]
-        st.success(f"La hora de la puesta de sol en {ubicacion} el {fecha.strftime('%Y-%m-%d')} es a las {sunset_time}")
+        sunset_time_utc = data["results"]["sunset"]
+        
+        # Convertir la hora de la puesta de sol a un objeto datetime
+        sunset_datetime_utc = datetime.strptime(sunset_time_utc, '%Y-%m-%dT%H:%M:%S+00:00')
+
+        # Convertir la hora de la puesta de sol a la zona horaria del usuario
+        sunset_datetime_user_timezone = sunset_datetime_utc.strftime('%Y-%m-%d %H:%M:%S')
+
+        print('UTC: ', sunset_datetime_user_timezone)
+
+        # Pasar de utc a utc+1
+        sunset_datetime_user_timezone_1 = datetime.strptime(sunset_datetime_user_timezone, '%Y-%m-%d %H:%M:%S') + timedelta(hours=1)
+
+        print('UTC +1 : ', sunset_datetime_user_timezone_1)
+
+        
+        st.success(f"La hora de la puesta de sol en {ubicacion} el {fecha.strftime('%Y-%m-%d')} es a las {sunset_datetime_user_timezone_1}")
     else:
         st.error("Hubo un error al obtener la información. Por favor, inténtalo de nuevo.")
