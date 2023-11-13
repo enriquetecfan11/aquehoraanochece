1e9687820244a6243257f122c50b45b7fd29452a
Kike Rodriguez Vela <enriquetecfan@Kikes-MacBook-Pro.local>
2023-11-13 21:25:20 +0100

    dev things

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
-import requests
 from datetime import datetime
+import requests
+import folium
+from streamlit_folium import folium_static
 
 # Página principal
 st.title("¿A qué hora anochece?")
@@ -17,14 +19,25 @@ if st.button("Detectar ubicación"):
 # Agregar un campo de fecha
 fecha = st.date_input("Seleccione la fecha:", datetime.today())
 
+# Mapa interactivo
+mapa = folium.Map(location=[0, 0], zoom_start=2)
+
 # Botón para obtener la hora de la puesta de sol
 if st.button("Obtener hora de la puesta de sol"):
     # Construir la URL para la API de sunrise-sunset.org
     api_url = f"https://api.sunrise-sunset.org/json?lat=0&lng=0&date={fecha.strftime('%Y-%m-%d')}&formatted=0"
     if ubicacion:
-        # Si se proporciona la ubicación, intentar obtener las coordenadas usando algún servicio de geocodificación
-        # En este ejemplo, simplemente usamos 0,0 como coordenadas si no hay detección de ubicación.
-        api_url = f"https://api.sunrise-sunset.org/json?lat=0&lng=0&date={fecha.strftime('%Y-%m-%d')}&formatted=0"
+        # Intentar obtener las coordenadas usando algún servicio de geocodificación
+        try:
+            response = requests.get(f"https://nominatim.openstreetmap.org/search?format=json&q={ubicacion}")
+            data = response.json()
+            if data:
+                lat, lon = float(data[0]["lat"]), float(data[0]["lon"])
+                # Actualizar el mapa con la nueva ubicación
+                mapa.location = [lat, lon]
+                mapa.zoom_start = 10
+        except Exception as e:
+            st.error(f"Error al obtener las coordenadas: {e}")
 
     # Hacer la solicitud a la API
     response = requests.get(api_url)
@@ -36,3 +49,6 @@ if st.button("Obtener hora de la puesta de sol"):
         st.success(f"La hora de la puesta de sol en {ubicacion} el {fecha.strftime('%Y-%m-%d')} es a las {sunset_time}")
     else:
         st.error("Hubo un error al obtener la información. Por favor, inténtalo de nuevo.")
+
+# Mostrar el mapa en la interfaz de Streamlit
+folium_static(mapa)
