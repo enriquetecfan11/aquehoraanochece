62d1abb1cb6166646a6231b80732402a3fd40500
Kike Rodriguez Vela <enriquetecfan@Kikes-MacBook-Pro.local>
2023-11-26 15:59:21 +0100

    fx map

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from datetime import datetime, timedelta
 import requests
-import folium
 from streamlit_folium import folium_static
 from deep_translator import GoogleTranslator
+
 
 st.set_page_config(
     page_title='A qué hora anochece',
@@ -67,36 +67,38 @@ with col1:
             sunset_datetime_user_timezone = sunset_datetime_utc.strftime('%Y-%m-%d %H:%M:%S')
             sunset_datetime_user_timezone_1 = datetime.strptime(sunset_datetime_user_timezone, '%Y-%m-%d %H:%M:%S') + timedelta(hours=1)
             st.success(f"La hora de la puesta de sol en {ubicacion} el {fecha.strftime('%Y-%m-%d')} es a las {sunset_datetime_user_timezone_1} UTC+1")
+        
+            # Obtener datos meteorológicos usando la API de OpenWeatherMap
+            api_key = '959d55cbfd41bca8951a491bde080a8c'
+            weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
 
-            if lat is not None and lon is not None:
-              # Obtener datos meteorológicos usando la API de OpenWeatherMap
-              api_key = '959d55cbfd41bca8951a491bde080a8c'
-              weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
-
-              weather_response = requests.get(weather_url)
-              weather_data = weather_response.json()
+            weather_response = requests.get(weather_url)
+            weather_data = weather_response.json()
 
             if weather_data['cod'] == 200:
-                weather_description = weather_data['weather'][0]['description'].capitalize()
-                weather_description_translated = GoogleTranslator(source='auto', target='es').translate(weather_description)
-                temperature = weather_data['main']['temp']
-                temperature_celsius = temperature - 273.15  # Convertir a Celsius
+              weather_description = weather_data['weather'][0]['description'].capitalize()
+              weather_description_translated = GoogleTranslator(source='auto', target='es').translate(weather_description)
+              temperature = weather_data['main']['temp']
+              temperature_celsius = temperature - 273.15  # Convertir a Celsius
+      
+              st.markdown("### Información meteorológica en tiempo real:")
+              st.markdown(f"El clima en {ubicacion} es {weather_description_translated} con una temperatura de {temperature_celsius:.2f}°C")
+        
 
-                st.markdown("---")
-                st.markdown("### Información meteorológica")
-                st.markdown(f"El clima en {ubicacion} es {weather_description_translated} con una temperatura de {temperature_celsius:.2f}°C")
-                st.markdown("---")
-            else:
-                st.error("No se pudo obtener la información del clima.")
+
+        
         else:
             st.error("Hubo un error al obtener la información. Por favor, inténtalo de nuevo.")
             st.write("Hubo un error al obtener la información. Por favor, inténtalo de nuevo.")
 
 
 with col2:
-    if lat is not None and lon is not None:
-        m = folium.Map(location=[lat, lon], zoom_start=15)
-        folium.Marker([lat, lon], popup=ubicacion).add_to(m)
-        folium_static(m)
-    else:
-        st.warning("Por favor, ingrese una ubicación válida.")
+  if lat is not None and lon is not None:
+
+      # Obtener mapa usando la API de OpenStreetMap con un zoom de 12x
+    st.markdown("### Mapa de la ubicación:")
+    map_url = f"https://www.openstreetmap.org/export/embed.html?bbox={lon-0.05}%2C{lat-0.05}%2C{lon+0.05}%2C{lat+0.05}&amp;layer=mapnik&amp;marker={lat}%2C{lon}"
+    st.components.v1.iframe(map_url, height=400)
+  
+  else:
+    st.error("Ubicación no especificada. Por favor, ingrese una ubicación válida.")
