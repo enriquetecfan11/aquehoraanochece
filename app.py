0e4432f2235963410bd904997d1400afcce121f1
Enrique Rodriguez Vela <enriquemondejar15@gmail.com>
2024-11-27 12:17:34 +0100

    Updated

--- app.py
+++ app.py
@@ -41,47 +41,65 @@ lat, lon = None, None
 with col1:
     ubicacion = st.text_input("Ingrese su ubicación:")
     fecha = st.date_input("Seleccione la fecha:", datetime.today())
-    #st.markdown("---")
     if st.button("Obtener hora de la puesta de sol", key='get_sunset_time'):
-        if ubicacion:
-            try:
-                response = requests.get(f"https://nominatim.openstreetmap.org/search?format=json&q={ubicacion}")
-                data = response.json()
-                lat, lon = float(data[0]["lat"]), float(data[0]["lon"])
-                api_url = f"https://api.sunrise-sunset.org/json?lat={lat}&lng={lon}&date={fecha.strftime('%Y-%m-%d')}&formatted=0"
-            except Exception as e:
-                st.error(f"Error al obtener las coordenadas: {e}")
-        else:
+        if not ubicacion:
             st.error("Ubicación no especificada. Por favor, ingrese una ubicación válida.")
             st.stop()
 
-        response = requests.get(api_url)
-        data = response.json()
+        try:
+            # Obtener coordenadas usando Nominatim
+            response = requests.get(f"https://nominatim.openstreetmap.org/search?format=json&q={ubicacion}")
+            response.raise_for_status()
+            data = response.json()
+            if not data:
+                st.error("No se encontraron coordenadas para la ubicación proporcionada.")
+                st.stop()
 
-        if data["status"] == "OK":
+            lat, lon = float(data[0]["lat"]), float(data[0]["lon"])
+            api_url = f"https://api.sunrise-sunset.org/json?lat={lat}&lng={lon}&date={fecha.strftime('%Y-%m-%d')}&formatted=0"
+        except Exception as e:
+            st.error(f"Error al obtener las coordenadas: {e}")
+            st.stop()
+
+        try:
+            # Obtener datos de la puesta de sol
+            response = requests.get(api_url)
+            response.raise_for_status()
+            data = response.json()
+
+            if data["status"] != "OK":
+                st.error("No se pudo obtener la información de la puesta de sol. Intente nuevamente.")
+                st.stop()
+
             sunset_time_utc = data["results"]["sunset"]
             sunset_datetime_utc = datetime.strptime(sunset_time_utc, '%Y-%m-%dT%H:%M:%S+00:00')
-            sunset_datetime_user_timezone = sunset_datetime_utc.strftime('%Y-%m-%d %H:%M:%S')
-            sunset_datetime_user_timezone_1 = datetime.strptime(sunset_datetime_user_timezone, '%Y-%m-%d %H:%M:%S') + timedelta(hours=1)
-            st.success(f"La hora de la puesta de sol en {ubicacion} el {fecha.strftime('%Y-%m-%d')} es a las {sunset_datetime_user_timezone_1} UTC+1")
-        
-            # Obtener datos meteorológicos usando la API de OpenWeatherMap
+            sunset_datetime_user_timezone = sunset_datetime_utc + timedelta(hours=1)  # UTC+1
+            st.success(f"La hora de la puesta de sol en {ubicacion} el {fecha.strftime('%Y-%m-%d')} es a las {sunset_datetime_user_timezone.strftime('%H:%M:%S')} UTC+1")
+        except Exception as e:
+            st.error(f"Error al obtener la información de la puesta de sol: {e}")
+            st.stop()
+
+        try:
+            # Obtener información meteorológica
             api_key = '959d55cbfd41bca8951a491bde080a8c'
             weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
-
             weather_response = requests.get(weather_url)
+            weather_response.raise_for_status()
             weather_data = weather_response.json()
 
-            if weather_data['cod'] == 200:
-              weather_description = weather_data['weather'][0]['description'].capitalize()
-              temperature = weather_data['main']['temp']
-              temperature_celsius = temperature - 273.15  # Convertir a Celsius
-      
-              st.markdown("### Información meteorológica en tiempo real:")
-              st.markdown(f"El clima en {ubicacion} es {weather_description} con una temperatura de {temperature_celsius:.2f}°C")
-        else:
-            st.error("Hubo un error al obtener la información. Por favor, inténtalo de nuevo.")
-            st.write("Hubo un error al obtener la información. Por favor, inténtalo de nuevo.")
+            if weather_data['cod'] != 200:
+                st.error("No se pudo obtener información meteorológica. Intente nuevamente.")
+                st.stop()
+
+            weather_description = weather_data['weather'][0]['description'].capitalize()
+            temperature = weather_data['main']['temp']
+            temperature_celsius = temperature - 273.15  # Convertir a Celsius
+
+            st.markdown("### Información meteorológica en tiempo real:")
+            st.markdown(f"El clima en {ubicacion} es {weather_description} con una temperatura de {temperature_celsius:.2f}°C")
+        except Exception as e:
+            st.error(f"Error al obtener la información meteorológica: {e}")
+
 
 
 with col2:
